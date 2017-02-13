23944bd87a7032804b442c6b92c34ba25fcd42cf
Sally M <sally.mitrofanova@gmail.com>
2017-02-12 16:26:37 -0800

    cleaned up syntax

--- sql_interview_prep.sql
+++ sql_interview_prep.sql
@@ -11,17 +11,12 @@ CREATE TABLE salesperson (
 );
 
 INSERT INTO salesperson (id, name, age, salary)
-VALUES (1, 'Abe', 61, 140000);
-INSERT INTO salesperson (id, name, age, salary)
-VALUES (2, 'Bob', 34, 44000);
-INSERT INTO salesperson (id, name, age, salary)
-VALUES (5, 'Chris', 34, 40000);
-INSERT INTO salesperson (id, name, age, salary)
-VALUES (7, 'Dan', 41, 52000);
-INSERT INTO salesperson (id, name, age, salary)
-VALUES (8, 'Ken', 57, 115000);
-INSERT INTO salesperson (id, name, age, salary)
-VALUES (11, 'Joe', 38, 38000);
+VALUES (1, 'Abe', 61, 140000),
+       (2, 'Bob', 34, 44000),
+       (5, 'Chris', 34, 40000),
+       (7, 'Dan', 41, 52000),
+       (8, 'Ken', 57, 115000),
+       (11, 'Joe', 38, 38000);
 
 
 CREATE TABLE customer (
@@ -33,13 +28,10 @@ CREATE TABLE customer (
 );
 
 INSERT INTO customer (id, name, city, industry_type)
-VALUES (4, 'Samsonic', 'pleasant', 'J');
-INSERT INTO customer (id, name, city, industry_type)
-VALUES (6, 'Panasung', 'oaktown', 'J');
-INSERT INTO customer (id, name, city, industry_type)
-VALUES (7, 'Samony', 'jackson', 'B');
-INSERT INTO customer (id, name, city, industry_type)
-VALUES (9, 'Orange', 'jackson', 'B');
+VALUES (4, 'Samsonic', 'pleasant', 'J'),
+       (6, 'Panasung', 'oaktown', 'J'),
+       (7, 'Samony', 'jackson', 'B'),
+       (9, 'Orange', 'jackson', 'B');
 
 
 CREATE TABLE orders (
@@ -52,19 +44,13 @@ CREATE TABLE orders (
 );
 
 INSERT INTO orders (num, order_date, cust_id, salesperson_id, amount)
-VALUES (10, '1998-08-02', 4, 2, 540);
-INSERT INTO orders (num, order_date, cust_id, salesperson_id, amount)
-VALUES (20, '1999-01-30', 4, 8, 1800);
-INSERT INTO orders (num, order_date, cust_id, salesperson_id, amount)
-VALUES (30, '1995-07-14', 9, 1, 460);
-INSERT INTO orders (num, order_date, cust_id, salesperson_id, amount)
-VALUES (40, '1998-01-29', 7, 2, 2400);
-INSERT INTO orders (num, order_date, cust_id, salesperson_id, amount)
-VALUES (50, '1998-02-03', 6, 7, 600);
-INSERT INTO orders (num, order_date, cust_id, salesperson_id, amount)
-VALUES (60, '1998-03-02', 6, 7, 720);
-INSERT INTO orders (num, order_date, cust_id, salesperson_id, amount)
-VALUES (70, '1998-05-06', 9, 7, 150);
+VALUES (10, '1998-08-02', 4, 2, 540),
+       (20, '1999-01-30', 4, 8, 1800),
+       (30, '1995-07-14', 9, 1, 460),
+       (40, '1998-01-29', 7, 2, 2400),
+       (50, '1998-02-03', 6, 7, 600),
+       (60, '1998-03-02', 6, 7, 720),
+       (70, '1998-05-06', 9, 7, 150);
 
 
 --a. The names of all salespeople that have an order with Samsonic.
@@ -155,21 +141,22 @@ CREATE TABLE user (
   PRIMARY KEY(user_id)
 );
 
-INSERT INTO user (name, phone_num) VALUES ('Abe', '1234567890');
-INSERT INTO user (name, phone_num) VALUES ('Bob', '9876543210');
-INSERT INTO user (name, phone_num) VALUES ('Chris', '6789012345');
-INSERT INTO user (name, phone_num) VALUES ('Danny', '5678901234');
-INSERT INTO user (name, phone_num) VALUES ('Emily', '6543298710');
-INSERT INTO user (name, phone_num) VALUES ('Fran', '6789012345');
-INSERT INTO user (name, phone_num) VALUES ('George', '9012567834');
-INSERT INTO user (name, phone_num) VALUES ('Harry', '5432698710');
-INSERT INTO user (name, phone_num) VALUES ('Ian', '7890162345');
-INSERT INTO user (name, phone_num) VALUES ('Jane', '8901672345');
-INSERT INTO user (name, phone_num) VALUES ('Kevin', '2348901675');
-INSERT INTO user (name, phone_num) VALUES ('Lilly', '3489021675');
-INSERT INTO user (name, phone_num) VALUES ('Mary', '4890216375');
-INSERT INTO user (name, phone_num) VALUES ('Nick', '1637489025');
-INSERT INTO user (name, phone_num) VALUES ('Oliver', '3748916025');
+INSERT INTO user (name, phone_num)
+VALUES ('Abe', '1234567890'),
+       ('Bob', '9876543210'),
+       ('Chris', '6789012345'),
+       ('Danny', '5678901234'),
+       ('Emily', '6543298710'),
+       ('Fran', '6789012345'),
+       ('George', '9012567834'),
+       ('Harry', '5432698710'),
+       ('Ian', '7890162345'),
+       ('Jane', '8901672345'),
+       ('Kevin', '2348901675'),
+       ('Lilly', '3489021675'),
+       ('Mary', '4890216375'),
+       ('Nick', '1637489025'),
+       ('Oliver', '3748916025');
 
 CREATE TABLE userHistory (
   rec_id INT NOT NULL AUTO_INCREMENT,
@@ -179,42 +166,43 @@ CREATE TABLE userHistory (
   PRIMARY KEY(rec_id)
 );
 
-INSERT INTO userHistory (user_id, rec_date, action) VALUES (3, '2017-01-01', 'logged_on');
-INSERT INTO userHistory (user_id, rec_date, action) VALUES (5, '2017-01-01', 'logged_on');
-INSERT INTO userHistory (user_id, rec_date, action) VALUES (1, '2017-01-10', 'logged_on');
-INSERT INTO userHistory (user_id, rec_date, action) VALUES (7, '2017-01-10', 'off');
-INSERT INTO userHistory (user_id, rec_date, action) VALUES (6, '2017-01-15', 'logged_on');
-INSERT INTO userHistory (user_id, rec_date, action) VALUES (1, '2017-01-15', 'off');
-INSERT INTO userHistory (user_id, rec_date, action) VALUES (4, '2017-01-17', 'logged_on');
-INSERT INTO userHistory (user_id, rec_date, action) VALUES (5, '2017-01-17', 'logged_on');
-INSERT INTO userHistory (user_id, rec_date, action) VALUES (10, '2017-01-17', 'off');
-INSERT INTO userHistory (user_id, rec_date, action) VALUES (2, '2017-01-17', 'logged_on');
-INSERT INTO userHistory (user_id, rec_date, action) VALUES (2, '2017-01-19', 'off');
-INSERT INTO userHistory (user_id, rec_date, action) VALUES (1, '2017-01-19', 'logged_on');
-INSERT INTO userHistory (user_id, rec_date, action) VALUES (10, '2017-01-20', 'logged_on');
-INSERT INTO userHistory (user_id, rec_date, action) VALUES (2, '2017-01-20', 'logged_on');
-INSERT INTO userHistory (user_id, rec_date, action) VALUES (3, '2017-01-20', 'logged_on');
-INSERT INTO userHistory (user_id, rec_date, action) VALUES (7, '2017-01-20', 'off');
-INSERT INTO userHistory (user_id, rec_date, action) VALUES (1, '2017-01-22', 'logged_on');
-INSERT INTO userHistory (user_id, rec_date, action) VALUES (3, '2017-01-22', 'off');
-INSERT INTO userHistory (user_id, rec_date, action) VALUES (13, '2017-01-22', 'logged_on');
-INSERT INTO userHistory (user_id, rec_date, action) VALUES (2, '2017-01-24', 'off');
-INSERT INTO userHistory (user_id, rec_date, action) VALUES (7, '2017-01-24', 'logged_on');
-INSERT INTO userHistory (user_id, rec_date, action) VALUES (5, '2017-01-25', 'logged_on');
-INSERT INTO userHistory (user_id, rec_date, action) VALUES (10, '2017-01-25', 'logged_on');
-INSERT INTO userHistory (user_id, rec_date, action) VALUES (2, '2017-01-28', 'off');
-INSERT INTO userHistory (user_id, rec_date, action) VALUES (2, '2017-01-29', 'logged_on');
-INSERT INTO userHistory (user_id, rec_date, action) VALUES (8, '2017-02-01', 'logged_on');
-INSERT INTO userHistory (user_id, rec_date, action) VALUES (3, '2017-02-01', 'off');
-INSERT INTO userHistory (user_id, rec_date, action) VALUES (6, '2017-02-01', 'logged_on');
-INSERT INTO userHistory (user_id, rec_date, action) VALUES (2, '2017-02-02', 'off');
-INSERT INTO userHistory (user_id, rec_date, action) VALUES (1, '2017-02-02', 'logged_on');
-INSERT INTO userHistory (user_id, rec_date, action) VALUES (10, '2017-02-02', 'logged_on');
-INSERT INTO userHistory (user_id, rec_date, action) VALUES (7, '2017-02-02', 'logged_on');
-INSERT INTO userHistory (user_id, rec_date, action) VALUES (10, '2017-02-05', 'logged_on');
-INSERT INTO userHistory (user_id, rec_date, action) VALUES (2, '2017-02-05', 'off');
-INSERT INTO userHistory (user_id, rec_date, action) VALUES (8, '2017-02-05', 'logged_on');
-INSERT INTO userHistory (user_id, rec_date, action) VALUES (1, '2017-02-05', 'logged_on');
+INSERT INTO userHistory (user_id, rec_date, action)
+VALUES (3, '2017-01-01', 'logged_on'),
+        (5, '2017-01-01', 'logged_on'),
+        (1, '2017-01-10', 'logged_on'),
+        (7, '2017-01-10', 'off'),
+        (6, '2017-01-15', 'logged_on'),
+        (1, '2017-01-15', 'off'),
+        (4, '2017-01-17', 'logged_on'),
+        (5, '2017-01-17', 'logged_on'),
+        (10, '2017-01-17', 'off'),
+        (2, '2017-01-17', 'logged_on'),
+        (2, '2017-01-19', 'off'),
+        (1, '2017-01-19', 'logged_on'),
+        (10, '2017-01-20', 'logged_on'),
+        (2, '2017-01-20', 'logged_on'),
+        (3, '2017-01-20', 'logged_on'),
+        (7, '2017-01-20', 'off'),
+        (1, '2017-01-22', 'logged_on'),
+        (3, '2017-01-22', 'off'),
+        (13, '2017-01-22', 'logged_on'),
+        (2, '2017-01-24', 'off'),
+        (7, '2017-01-24', 'logged_on'),
+        (5, '2017-01-25', 'logged_on'),
+        (10, '2017-01-25', 'logged_on'),
+        (2, '2017-01-28', 'off'),
+        (2, '2017-01-29', 'logged_on'),
+        (8, '2017-02-01', 'logged_on'),
+        (3, '2017-02-01', 'off'),
+        (6, '2017-02-01', 'logged_on'),
+        (2, '2017-02-02', 'off'),
+        (1, '2017-02-02', 'logged_on'),
+        (10, '2017-02-02', 'logged_on'),
+        (7, '2017-02-02', 'logged_on'),
+        (10, '2017-02-05', 'logged_on'),
+        (2, '2017-02-05', 'off'),
+        (8, '2017-02-05', 'logged_on'),
+        (1, '2017-02-05', 'logged_on');
 
 
 /*1. Write a SQL query that returns the name, phone number and most recent date for any user that has logged in over the last 30 days
