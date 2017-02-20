316c2b4884e19d09c754b5950676da77c2c13b67
Sally M <sally.mitrofanova@gmail.com>
2017-02-20 15:34:50 -0800

    more practice, string manipulation

--- hacker_rank_problems.sql
+++ hacker_rank_problems.sql
@@ -23,3 +23,13 @@ FROM (
       LIMIT 1
      ) as longest
 ;
+
+/*
+Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels.
+Your result cannot contain duplicates.
+*/
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY RLIKE '^[^aeiouAEIOU]'
+OR CITY RLIKE '[^aeiouAEIOU]$'
+;
