4f7aa47c1642c4551935fd3d0c01698ff62a92fe
Sally M <sally.mitrofanova@gmail.com>
2017-02-21 12:08:29 -0800

    finding a median

--- hacker_rank_problems.sql
+++ hacker_rank_problems.sql
@@ -36,7 +36,7 @@ OR CITY RLIKE '[^aeiouAEIOU]$'
 
 
 /*
-Query the Name of any student in STUDENTS who scored higher than  Marks.
+Query the Name of any student in STUDENTS who scored higher than 75 Marks.
 Order your output by the last three characters of each name.
 If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
 */
@@ -44,3 +44,19 @@ SELECT Name
 FROM STUDENTS
 WHERE Marks > 75
 ORDER BY RIGHT(Name, 3) ASC, ID;
+
+/*
+A median is defined as a number separating the higher half of a data set from the lower half.
+Query the median of the Northern Latitudes (LAT_N) from STATION and round your answer to  decimal places.
+
+-- to get index of the middle number:
+select count(*)/2
+from station;
+
+*/
+
+SELECT ROUND(LAT_N, 4) as median
+FROM STATION
+ORDER BY LAT_N
+LIMIT 249, 1
+;
