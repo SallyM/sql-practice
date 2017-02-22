8950d59bc0f473ad29be8cce0c9c82fdc75e5a39
Sally M <sally.mitrofanova@gmail.com>
2017-02-22 12:08:49 -0800

    string manipulation

--- hacker_rank_problems.sql
+++ hacker_rank_problems.sql
@@ -102,9 +102,9 @@ ORDER BY comb.f_salary;
 Write a query identifying the type of each record in the TRIANGLES table using its three side lengths. Output one of the following statements for each record in the table:
 
 Not A Triangle: The given values of A, B, and C don't form a triangle.
-Equilateral: It's a triangle with  sides of equal length.
-Isosceles: It's a triangle with  sides of equal length.
-Scalene: It's a triangle with  sides of differing lengths.
+Equilateral: It's a triangle with 3 sides of equal length.
+Isosceles: It's a triangle with 2 sides of equal length.
+Scalene: It's a triangle with 3 sides of differing lengths.
 */
 
 SELECT (CASE
@@ -119,4 +119,30 @@ SELECT (CASE
         ELSE 'Not A Triangle'
        END) as figure_type
 FROM TRIANGLES
+;
+
+
+/*
+Generate the following two result sets:
+
+1. Query an alphabetically ordered list of all names in OCCUPATIONS, immediately followed by the first letter of each profession as a parenthetical (i.e.: enclosed in parentheses). For example: AnActorName(A), ADoctorName(D), AProfessorName(P), and ASingerName(S).
+2. Query the number of ocurrences of each occupation in OCCUPATIONS. Sort the occurrences in ascending order, and output them in the following format:
+        There are total [occupation_count] [occupation]s.
+   where [occupation_count] is the number of occurrences of an occupation in OCCUPATIONS and [occupation] is the lowercase occupation name. If more than one Occupation has the same [occupation_count], they should be ordered alphabetically.
+
+Note: There will be at least two entries in the table for each type of occupation.
+*/
+
+SELECT CONCAT(Name, '(', LEFT(Occupation, 1), ')') as Name_prof
+FROM OCCUPATIONS
+ORDER BY Name_prof
+;
+
+SELECT CONCAT('There are total ', cnt, " ", lower(Occupation), 's.') AS totals
+FROM (SELECT COUNT(*) as cnt,
+             Occupation
+    FROM OCCUPATIONS
+    GROUP BY Occupation
+    ORDER BY Occupation) as groupped
+ORDER BY totals
 ;
