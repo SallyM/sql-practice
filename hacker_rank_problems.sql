5f72e34d8150c1bc32a34e95ff7cf0cabbe5aee4
Sally M <sally.mitrofanova@gmail.com>
2017-02-21 17:24:59 -0800

    multiple joins

--- hacker_rank_problems.sql
+++ hacker_rank_problems.sql
@@ -72,3 +72,28 @@ FROM
 WHERE Start_Date < End_Date
 GROUP BY Start_Date
 ORDER BY DATEDIFF(End_Date, Start_Date), Start_Date
+
+/*
+You are given three tables: Students, Friends and Packages.
+Students contains two columns: ID and Name.
+Friends contains two columns: ID and Friend_ID (ID of the ONLY best friend).
+Packages contains two columns: ID and Salary (offered salary in $ thousands per month).
+Write a query to output the names of those students whose best friends got offered a higher salary than them.
+Names must be ordered by the salary amount offered to the best friends.
+It is guaranteed that no two students got same salary offer.
+*/
+SELECT Students.Name
+FROM Students
+JOIN
+    (SELECT f.ID as id,
+           s.Salary as salary,
+           f.Friend_id as f_id,
+           fs.Salary as f_salary
+    FROM Friends f
+    JOIN Packages AS s
+    ON f.ID = s.ID
+    JOIN Packages AS fs
+    ON f.Friend_ID = fs.ID) as comb
+ON Students.ID = comb.id
+WHERE comb.f_salary > comb.salary
+ORDER BY comb.f_salary;
