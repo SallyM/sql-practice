478f9cf412e6503d3a1ba873f1917d41fac9b44e
Sally M <sally.mitrofanova@gmail.com>
2017-03-04 20:18:52 -0800

    more practice

--- sql_interview_prep.sql
+++ sql_interview_prep.sql
@@ -303,7 +303,7 @@ WHERE amount = max_order;
 */
 
 /*
-Given timestamps of logins, figure out how many people on Facebook were active all seven days of a week on a mobile phone.
+Given timestamps of logins, figure out how many people were active all seven days of a week on a mobile phone.
 */
 -- not very clear if asking
 -- a) how many users (groupped by day) were active on mobile in general each day of week (e.g. 5 users were active on Monday, 10 on Tuesday, etc.) or
@@ -395,3 +395,232 @@ FROM
 GROUP BY active_days
 HAVING active_days =7
 ;
+
+/*
+How do you determine what product was used most by the non-employee users for the last quarter?
+*/
+
+-- all info in one table
+CREATE TABLE products_used (
+  user_id INT NOT NULL
+, date_of_use DATE
+, employee_status VARCHAR(20)
+, product VARCHAR(20)
+, num_times_used INT
+);
+
+INSERT INTO products_used (user_id, date_of_use, employee_status, product, num_times_used) VALUES
+  (100, '2016-09-15', 'employee', 'profile', 5)
+, (200, '2016-09-15', 'non-employee', 'profile', 10)
+, (300, '2016-09-15', 'employee', 'profile', 10)
+, (300, '2016-09-15', 'employee', 'groups', 20)
+, (400, '2016-09-15', 'employee', 'groups', 20)
+, (100, '2016-09-20', 'employee', 'profile', 2)
+, (200, '2016-09-20', 'non-employee', 'groups', 2)
+, (500, '2016-09-21', 'non-employee', 'photos', 15)
+, (200, '2016-09-21', 'non-employee', 'groups', 2)
+, (500, '2016-09-22', 'non-employee', 'groups', 5)
+, (200, '2016-09-22', 'non-employee', 'events', 5)
+, (500, '2016-09-24', 'non-employee', 'search', 10)
+, (300, '2016-09-25', 'employee', 'events', 20)
+, (400, '2016-09-25', 'employee', 'photos', 23)
+, (400, '2016-09-25', 'employee', 'profile', 12)
+, (400, '2016-09-25', 'employee', 'groups', 5)
+, (400, '2016-09-25', 'employee', 'search', 10)
+, (100, '2016-10-05', 'employee', 'profile', 5)
+, (200, '2016-10-05', 'non-employee', 'profile', 10)
+, (300, '2016-10-05', 'employee', 'profile', 10)
+, (300, '2016-10-15', 'employee', 'groups', 10)
+, (400, '2016-10-15', 'employee', 'groups', 20)
+, (100, '2016-10-20', 'employee', 'profile', 29)
+, (200, '2016-10-20', 'non-employee', 'groups', 2)
+, (500, '2016-10-22', 'non-employee', 'photos', 15)
+, (200, '2016-10-23', 'non-employee', 'groups', 5)
+, (500, '2016-10-24', 'non-employee', 'groups', 8)
+, (200, '2016-10-24', 'non-employee', 'events', 5)
+, (500, '2016-10-24', 'non-employee', 'search', 10)
+, (300, '2016-10-25', 'employee', 'events', 20)
+, (400, '2016-10-25', 'employee', 'photos', 23)
+, (400, '2016-10-25', 'employee', 'profile', 12)
+, (400, '2016-10-25', 'employee', 'groups', 5)
+, (400, '2016-10-25', 'employee', 'search', 10)
+, (100, '2016-11-05', 'employee', 'profile', 5)
+, (200, '2016-11-05', 'non-employee', 'profile', 10)
+, (300, '2016-11-05', 'employee', 'profile', 10)
+, (300, '2016-11-15', 'employee', 'groups', 20)
+, (400, '2016-11-15', 'employee', 'groups', 20)
+, (100, '2016-11-20', 'employee', 'profile', 2)
+, (200, '2016-11-20', 'non-employee', 'groups', 2)
+, (500, '2016-11-22', 'non-employee', 'profile', 25)
+, (200, '2016-11-23', 'non-employee', 'groups', 2)
+, (500, '2016-11-24', 'non-employee', 'groups', 5)
+, (200, '2016-11-24', 'non-employee', 'events', 5)
+, (500, '2016-11-24', 'non-employee', 'search', 10)
+, (500, '2016-11-24', 'non-employee', 'photos', 25)
+, (200, '2016-11-24', 'non-employee', 'profile', 15)
+, (500, '2016-11-24', 'non-employee', 'search', 9)
+, (300, '2016-11-25', 'employee', 'events', 20)
+, (400, '2016-11-25', 'employee', 'photos', 23)
+, (400, '2016-11-25', 'employee', 'profile', 12)
+, (400, '2016-11-27', 'employee', 'groups', 5)
+, (200, '2016-11-27', 'non-employee', 'search', 10)
+, (100, '2016-12-05', 'employee', 'profile', 5)
+, (200, '2016-12-05', 'non-employee', 'profile', 10)
+, (300, '2016-12-05', 'employee', 'profile', 10)
+, (300, '2016-12-15', 'employee', 'groups', 20)
+, (400, '2016-12-15', 'employee', 'groups', 20)
+, (100, '2016-12-20', 'employee', 'profile', 2)
+, (200, '2016-12-20', 'non-employee', 'groups', 2)
+, (500, '2016-12-22', 'non-employee', 'profile', 25)
+, (200, '2016-12-23', 'non-employee', 'groups', 2)
+, (500, '2016-12-24', 'non-employee', 'profile', 5)
+, (200, '2016-12-24', 'non-employee', 'photos', 5)
+, (500, '2016-12-24', 'non-employee', 'search', 10)
+, (300, '2016-12-25', 'employee', 'events', 20)
+, (400, '2016-12-25', 'employee', 'photos', 23)
+, (400, '2016-12-25', 'employee', 'profile', 12)
+, (400, '2016-12-27', 'employee', 'groups', 5)
+, (400, '2016-12-27', 'employee', 'search', 10)
+, (200, '2017-01-01', 'non-employee', 'profile', 15)
+, (200, '2017-01-01', 'non-employee', 'photos', 155)
+, (500, '2017-01-01', 'non-employee', 'profile', 15)
+, (500, '2017-01-01', 'non-employee', 'photos', 105)
+, (200, '2017-01-02', 'non-employee', 'profile', 5)
+;
+
+SELECT product
+     , SUM(num_times_used) as total_usage
+FROM products_used
+WHERE employee_status = 'non-employee'
+AND date_of_use BETWEEN '2016-10-01' AND '2016-12-31'
+GROUP BY product
+ORDER BY total_usage DESC
+LIMIT 1;
+
+
+-- same question as above, but using separate tables
+CREATE TABLE products (
+  product_id INT NOT NULL PRIMARY KEY
+, product_name VARCHAR(20)
+);
+
+INSERT INTO products (product_id, product_name) VALUES
+  (1, 'profile')
+, (2, 'photos')
+, (3, 'groups')
+, (4, 'events')
+, (5, 'search')
+;
+
+CREATE TABLE user_table (
+  user_id INT NOT NULL PRIMARY KEY
+, user_name VARCHAR(30)
+, employee_status VARCHAR(30)
+);
+
+INSERT INTO user_table (user_id, user_name, employee_status) VALUES
+  (100, 'Jane Doe', 'employee')
+, (200, 'John Smith', 'non-employee')
+, (300, 'Victoria Brown', 'employee')
+, (400, 'William White', 'employee')
+, (500, 'Sally Sun', 'non-employee')
+;
+
+CREATE TABLE usage_stats (
+  user_id INT NOT NULL
+, date_of_use DATE
+, product INT NOT NULL
+, num_times_used INT
+);
+
+INSERT INTO usage_stats (user_id, date_of_use, product, num_times_used) VALUES
+  (100, '2016-09-15', 1, 5)
+, (200, '2016-09-15', 1, 10)
+, (300, '2016-09-15', 1, 10)
+, (300, '2016-09-15', 3, 20)
+, (400, '2016-09-15', 3, 20)
+, (100, '2016-09-20', 1, 2)
+, (200, '2016-09-20', 3, 2)
+, (500, '2016-09-21', 2, 15)
+, (200, '2016-09-21', 3, 2)
+, (500, '2016-09-22', 3, 5)
+, (200, '2016-09-22', 4, 5)
+, (500, '2016-09-24', 5, 10)
+, (300, '2016-09-25', 4, 20)
+, (400, '2016-09-25', 2, 23)
+, (400, '2016-09-25', 1, 12)
+, (400, '2016-09-25', 3, 5)
+, (400, '2016-09-25', 5, 10)
+, (100, '2016-10-05', 1, 5)
+, (200, '2016-10-05', 1, 10)
+, (300, '2016-10-05', 1, 10)
+, (300, '2016-10-15', 3, 10)
+, (400, '2016-10-15', 3, 20)
+, (100, '2016-10-20', 1, 29)
+, (200, '2016-10-20', 3, 2)
+, (500, '2016-10-22', 2, 15)
+, (200, '2016-10-23', 3, 5)
+, (500, '2016-10-24', 3, 8)
+, (200, '2016-10-24', 4, 5)
+, (500, '2016-10-24', 5, 10)
+, (300, '2016-10-25', 4, 20)
+, (400, '2016-10-25', 2, 23)
+, (400, '2016-10-25', 1, 12)
+, (400, '2016-10-25', 3, 5)
+, (400, '2016-10-25', 5, 10)
+, (100, '2016-11-05', 1, 5)
+, (200, '2016-11-05', 1, 10)
+, (300, '2016-11-05', 1, 10)
+, (300, '2016-11-15', 3, 20)
+, (400, '2016-11-15', 3, 20)
+, (100, '2016-11-20', 1, 2)
+, (200, '2016-11-20', 3, 2)
+, (500, '2016-11-22', 1, 25)
+, (200, '2016-11-23', 3, 2)
+, (500, '2016-11-24', 3, 5)
+, (200, '2016-11-24', 4, 5)
+, (500, '2016-11-24', 5, 10)
+, (500, '2016-11-24', 2, 25)
+, (200, '2016-11-24', 1, 15)
+, (500, '2016-11-24', 5, 9)
+, (300, '2016-11-25', 4, 20)
+, (400, '2016-11-25', 2, 23)
+, (400, '2016-11-25', 1, 12)
+, (400, '2016-11-27', 3, 5)
+, (200, '2016-11-27', 5, 10)
+, (100, '2016-12-05', 1, 5)
+, (200, '2016-12-05', 1, 10)
+, (300, '2016-12-05', 1, 10)
+, (300, '2016-12-15', 3, 20)
+, (400, '2016-12-15', 3, 20)
+, (100, '2016-12-20', 1, 2)
+, (200, '2016-12-20', 3, 2)
+, (500, '2016-12-22', 1, 25)
+, (200, '2016-12-23', 3, 2)
+, (500, '2016-12-24', 1, 5)
+, (200, '2016-12-24', 2, 5)
+, (500, '2016-12-24', 5, 10)
+, (300, '2016-12-25', 4, 20)
+, (400, '2016-12-25', 2, 23)
+, (400, '2016-12-25', 1, 12)
+, (400, '2016-12-27', 3, 5)
+, (400, '2016-12-27', 5, 10)
+, (200, '2017-01-01', 1, 15)
+, (200, '2017-01-01', 2, 155)
+, (500, '2017-01-01', 1, 15)
+, (500, '2017-01-01', 2, 105)
+, (200, '2017-01-02', 1, 5)
+;
+
+SELECT p.product_name name
+     , SUM(stats.num_times_used) num_times_used
+FROM products p
+LEFT JOIN usage_stats stats
+ON stats.product = p.product_id
+LEFT JOIN user_table u
+ON stats.user_id = u.user_id
+WHERE u.employee_status = 'non-employee'
+AND stats.date_of_use BETWEEN '2016-10-01' AND '2016-12-31'
+GROUP BY stats.product, name
+ORDER BY num_times_used DESC
+LIMIT 1;
